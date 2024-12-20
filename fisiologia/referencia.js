ecb2f6942838b47520c64a2d7338de632630ed18
Adérito Félix Quinamine <quinamine.aderitofelix@gmail.com>
2024-12-20 18:50:59 -0200

    HTML/JS: Desenvolvi a funcionalidade de referência que mostra o conteúdo da linha e coluna do input com focus.

--- fisiologia/referencia.js
+++ fisiologia/referencia.js
@@ -4,69 +4,37 @@ const referencia = {
         const linhaOutput = document.querySelector(".reference__output--indicador");
         const colOutput = document.querySelector(".reference__output--idade");
         const celulaComFocoEirmas = inputTarget.parentElement.children;
-
+        let CelulaComFocoIndex = 0;
+        for (let i = 0; i < celulaComFocoEirmas.length; i++) {
+            if(celulaComFocoEirmas[i] === inputTarget) CelulaComFocoIndex = i-1;
+        }
+        let tituloDaSeccao = inputTarget.parentElement.parentElement.getElementsByTagName("h2")[0];
         let indicadorLinear = inputTarget.parentElement.querySelector("span");
-        linhaOutput.textContent = indicadorLinear.textContent;
-        /* Seccoes
-        let isSection1 = inputTarget.parentElement.parentElement.matches(".ficha__seccao__body--1");
-        let isSection2 = inputTarget.parentElement.parentElement.matches(".ficha__seccao__body--2");
-        let isSection3 = inputTarget.parentElement.matches(".ficha__seccao__body--3");
-        let isSection4 = inputTarget.parentElement.parentElement.matches(".ficha__seccao__body--4");
-        let isSubSection4 = inputTarget.parentElement.matches(".ficha__seccao-4__pseudo-body")
-        if(isSection1) {
-            const indicadores = document.querySelectorAll(".ficha__seccao-1__subcol-de-indicadores--1 span");
-            const subindicadores = document.querySelectorAll(".ficha__seccao-1__subcol-de-indicadores--2 span");
-            let celulaFocadaIndex;
-            let indicadorIndex, subindicadorIndex;           
-            for(let i = 0; i < celulaComFocoEirmas.length; i++) {
-                if(inputTarget === celulaComFocoEirmas[i]) {
-                    celulaFocadaIndex = i;
-                    indicadorIndex = i < 4 ? 0
-                    : i < 8 ? 1
-                    : 2;
-                    subindicadorIndex = i;
-                }
+        let indicadoresColunares = document.querySelectorAll(".seccao-1__header__linha-de-indicadores-colunares span");
+        if(inputTarget.parentElement.matches(".seccao-2__body__tipo-de-resistencia")) {
+            indicadorLinear = document.querySelector(".seccao-1__header__col-variaveis"); // Aponta para um elemento vazio
+        }
+        linhaOutput.textContent = `${tituloDaSeccao.textContent}: ${indicadorLinear.textContent}`;
+        // Seccoes
+        let isSection2 = inputTarget.parentElement.parentElement.matches(".ficha__seccao--2");
+        let isSection3 = inputTarget.parentElement.parentElement.matches(".ficha__seccao--3");
+        let isSection4 = inputTarget.parentElement.parentElement.matches(".ficha__seccao--4");
+        if(isSection2) {
+            indicadoresColunares = document.querySelectorAll(".seccao-2__header__col2-da-linha-inferior span");
+            CelulaComFocoIndex++;
+            if(inputTarget.parentElement.matches(".ficha__seccao-2__grid-template-cols-4")) {
+                indicadoresColunares = "";
             }
-            indicadorOutput.textContent = `${indicadores[indicadorIndex].textContent}: ${subindicadores[subindicadorIndex].textContent}`;
-            faixaEtariaOutput.textContent = inputTarget.parentElement.dataset.faixaetaria;
-        } else if(isSubSection4) {
-            let tituloDaSeccao = document.querySelector(".ficha__seccao-4__subtitulo--2");
-            let indicadores = document.querySelectorAll(".indicadores-da-subseccao-4 span");
-            let celulaFocadaIndex;           
-            for(let i = 0; i < celulaComFocoEirmas.length; i++) {
-                if(inputTarget === celulaComFocoEirmas[i]) {
-                    celulaFocadaIndex = i;
-                }
+            else if(inputTarget.parentElement.matches(".seccao-2__body__tipo-de-resistencia")) {
+                indicadoresColunares = document.querySelectorAll(".seccao-2__pseudoheader__tipo-de-resistencia span");
             }
-            indicadorOutput.textContent = `${tituloDaSeccao.textContent}: ${indicadores[celulaFocadaIndex].textContent}`;
-            faixaEtariaOutput.textContent = "-";
-        } else {
-            let tituloDaSeccao;
-            if(isSection2) {
-                tituloDaSeccao = document.querySelector("#titulo-da-seccao-2");
-            }
-            else if(isSection3) {
-                tituloDaSeccao = document.querySelector("#titulo-da-seccao-3");
-                let indicador = `${tituloDaSeccao.textContent}: ${celulaComFocoEirmas[0].textContent}`;
-                indicadorOutput.textContent = indicador;
-                faixaEtariaOutput.textContent = "-";
-                return !1;
-            } else if(isSection4) {
-                tituloDaSeccao = document.querySelector("#titulo-da-seccao-4")
-            } else {
-                tituloDaSeccao = document.querySelector("#titulo-da-seccao-5");
-            }
-            let indicador = `${tituloDaSeccao.textContent}: ${celulaComFocoEirmas[0].textContent}`;
-            indicadorOutput.textContent = indicador;
-            let faixasEtarias = ["0 - 4","5 - 9","10 - 14","15 - 19","20 - 24","25 - 34","35 - 44","45 - 54","55 - 64","≥ 65"];
-            let celulaFocadaIndex;           
-            for(let i = 0; i < celulaComFocoEirmas.length; i++) {
-                if(inputTarget === celulaComFocoEirmas[i]) {
-                    celulaFocadaIndex = i - 1;
-                }
-            }
-            faixaEtariaOutput.textContent = faixasEtarias[celulaFocadaIndex];
-        }*/
+        } else if(isSection3) {
+            indicadoresColunares = document.querySelectorAll(".ficha__seccao-3__indicador-colunar");
+        } else if(isSection4) {
+            indicadoresColunares = document.querySelectorAll(".seccao-4__header__linha-de-indicadores-colunares span");
+        }
+        let indicadorColunar = indicadoresColunares[CelulaComFocoIndex];
+        colOutput.textContent = indicadorColunar.textContent;
     },
     retornarVazio() {
         const outputs = document.querySelectorAll(".reference__output");
